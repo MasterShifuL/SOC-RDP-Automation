994ed364c0ef7542e546059c854f7e98022c6db3
Achmad Amirul Mu'minin <derdor472@gmail.com>
2026-05-04 11:39:52 +0700

    Update disable_user.ps1

--- scripts/disable_user.ps1
+++ scripts/disable_user.ps1
@@ -1,1 +1,13 @@
+param(
+    [string]$username
+)
 
+Import-Module ActiveDirectory
+
+try {
+    Disable-ADAccount -Identity $username
+    Write-Output "SUCCESS: User $username disabled"
+}
+catch {
+    Write-Output "ERROR: $_"
+}
